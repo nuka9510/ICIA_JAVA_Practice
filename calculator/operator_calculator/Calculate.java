b8f01fd83e3a057d30c2dbba2561261a67876aba
nuka9510 <70128791+nuka9510@users.noreply.github.com>
2020-09-16 22:24:20 +0900

    Add files via upload

---
+++ calculator/operator_calculator/Calculate.java
@@ -0,0 +1,21 @@
+package operator_calculator;
+
+public class Calculate {
+	
+	public int plus(int num1, int num2) {
+		return num1+num2;
+	}
+	
+	public int minus(int num1, int num2) {
+		return num1-num2;
+	}
+	
+	public int product(int num1, int num2) {
+		return num1*num2;
+	}
+	
+	public int devide(int num1, int num2) {
+		return num1/num2;
+	}
+
+}
---
+++ calculator/operator_calculator/method.java
@@ -0,0 +1,13 @@
+package operator_calculator;
+
+public class method {
+	
+	public void method01 () {
+		
+	}
+	
+	public int method02() {
+		return 0;
+	}
+
+}
